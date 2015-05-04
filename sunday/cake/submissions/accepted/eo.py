b02228bb70a19536ea754c6caff632f51d87fc62
Erik Odenman <erik.odenman@gmail.com>
2015-05-04 23:21:27 +0200

    cake: Fixed bug in submission

--- sunday/cake/submissions/accepted/eo.py
+++ sunday/cake/submissions/accepted/eo.py
@@ -6,12 +6,12 @@ while ai < n:
     bi = ai
     while bi < n-1 and xy[bi][0] == xy[bi+1][0]:
         bi += 1
-    c = a if bi < n-1 else p
+    c = xy[ai][0] if bi < n-1 else p
     b = 1
     for i in range(ai, bi+1):
         d = xy[i][1] if i < bi else q
         print a,b,c,d
         b = d+1
-    a += 1
+    a = c+1
     ai = bi+1
 print 0
